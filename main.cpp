5dfdf5abb02b0303c1de2779cd6aada2e296769a
1037-s <k025g1037@g.neec.ac.jp>
2026-05-12 16:56:45 +0900

    00_06_exの課題を完了しました

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#pragma comment(lib,"d3d12.lib")
-#pragma comment(lib,"dxgi.lib")
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <cassert>
+#include <DbgHelp.h>
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"Dbghelp.lib")
+#include <strsafe.h>
 #include <Windows.h>
 #include <cstdint>
 #include <string>
@@ -13,8 +16,6 @@
 #include <fstream>
 // 時間を扱うライブラリ
 #include <chrono>
-
-
 
 std::wstring ConvertString(const std::string& str) {
 	if (str.empty()) {
@@ -75,13 +76,37 @@ void Log(std::ostream& os, const std::string& message) {
 	OutputDebugStringA(message.c_str());
 }
 
-
-
+static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception) {
+	// 中身はこれから埋める
+	// 時刻を取得して、時刻を名前に入れたファイルを作成。Dumpsディレクトリ以下に出力
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+	wchar_t filePath[MAX_PATH] = { 0 };
+	CreateDirectory(L"./Dumps", nullptr);
+	StringCchPrintfW(filePath, MAX_PATH, L"./Dumps/%04d-%02d%02d-%02d%02d.dmp", time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute);
+	HANDLE dumpFileHandle = CreateFile(filePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+	// processId(このexeのId)とクラッシュ(例外)が発生したthreadIdを取得する
+	DWORD processId = GetCurrentProcessId();
+	DWORD threadId = GetCurrentThreadId();
+	// 設定情報を入力
+	MINIDUMP_EXCEPTION_INFORMATION minidumpInformation{ 0 };
+	minidumpInformation.ThreadId = threadId;
+	minidumpInformation.ExceptionPointers = exception;
+	minidumpInformation.ClientPointers = TRUE;
+	// Dumpを出力。MiniDumpNormalは最低限の情報を出力するフラグ
+	MiniDumpWriteDump(GetCurrentProcess(), processId, dumpFileHandle, MiniDumpNormal, &minidumpInformation, nullptr, nullptr);
+	// 他に関連付けられているSEH例外ハンドラがあれば実行、通常はプロセスを実行する
+	return EXCEPTION_EXECUTE_HANDLER;
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	WNDCLASS wc{};
 
+	// 誰も催促しなかった場合に(Unhandled)、補足する関数を用意
+	// main関数始まってすぐに登録すると良い
+	SetUnhandledExceptionFilter(ExportDump);
+	
 	// ログのディレクトリを用意する
 	std::filesystem::create_directory("logs");
 
@@ -98,8 +123,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	std::string logFilePath = std::string("logs/") + dateString + ".log";
 	// ファイルを作って書き込み準備
 	std::ofstream logStream(logFilePath);
-
-
 
 	// ウィンドウプロシージャ
 	wc.lpfnWndProc = WindowProc;
@@ -119,8 +142,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	const int32_t kClientWidth = 1280;
 	const int32_t kClientHeight = 720;
 
-
-
 	// ウィンドウサイズを表す構造体にクライアント領域を入れる
 	RECT wrc = { 0,0,kClientWidth,kClientHeight };
 
@@ -129,7 +150,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 
 	//ウィンドウの生成
-
+	
 	HWND hwnd = CreateWindow(
 		wc.lpszClassName,					// 利用するクラス名
 		L"CG2",							// タイトルバーの文字	
