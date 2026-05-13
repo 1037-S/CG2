774e5d879d17896fc7618a708ff6d6cc8dec98cc
1037-s <k025g1037@g.neec.ac.jp>
2026-05-13 15:57:06 +0900

    01_01_の課題を完了 00_05_の課題を今度こそ完了

--- main.cpp
+++ main.cpp
@@ -179,14 +179,14 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	ShowWindow(hwnd, SW_SHOW);
 
 	// 変数から型を推論してくれる
-	int enemyhp = 100;
-	int texturePath = 0;
-	Log(std::format("enemyHp:{},texturePath:{}\n",enemyhp,texturePath));
+	//int enemyhp = 100;
+	//int texturePath = 0;
+	//Log(std::format("enemyHp:{},texturePath:{}\n",enemyhp,texturePath));
 
 	// string->wstring
-	std::wstring ConvertString(const std::string& str);
+	//std::wstring ConvertString(const std::string& str);
 	// wstring->string
-	std::string ConvertString(const std::wstring& str);
+	//std::string ConvertString(const std::wstring& str);
 
 
 	// DXGIファクトリーの生成
@@ -241,7 +241,41 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 			break;
 		}
 	}
+	// デバイスの生成がうまくいかなかったので起動できない
+	assert(device != nullptr);
+	// 初期化完了のログを出す
+	Log("Complete create D3D12Device!!!\n");
+#ifdef _DEBUG
+	ID3D12InfoQueue* infoQueue = nullptr;
+	if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&infoQueue))))
+	{
+		// ヤバいエラー時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
+		// エラー時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
+		// 警告時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
+		// 抑制するメッセージのID
+		D3D12_MESSAGE_ID denyIds[] = {
+		// Windows11でのDXGIデバッグプレイヤーとDX12デバッグレイヤーの相互作用バグによるエラーメッセージ
+		// https://stackoverflow.com/questions/69805245/directx-12-application-is-craghing-inwindows-11
+		D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE
+		};
+		// 抑制するレベル
+		D3D12_MESSAGE_SEVERITY severities[] = {D3D12_MESSAGE_SEVERITY_INFO};
+		D3D12_INFO_QUEUE_FILTER filter{};
+		filter.DenyList.NumIDs = _countof(denyIds);
+		filter.DenyList.pIDList = denyIds;
+		filter.DenyList.NumSeverities = _countof(severities);
+		filter.DenyList.pSeverityList = severities;
+		// 指定したメッセージの表示を抑制する
+		infoQueue->PushStorageFilter(&filter);
+		// 解放
+		infoQueue->Release();
+	}
 
+#endif // _DEBUG
+	
 	// コマンドキューを生成する
 	ID3D12CommandQueue* commandQueue = nullptr;
 	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
@@ -307,6 +341,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// ２つ目のディスクリプタハンドルを得る(自力で)
 	rtvHandles[1].ptr = rtvHandles[0].ptr + device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 	device->CreateRenderTargetView(swapChainResource[1], &rtvDesc, rtvHandles[1]);
+
+
+
 
 	MSG msg{};
 
